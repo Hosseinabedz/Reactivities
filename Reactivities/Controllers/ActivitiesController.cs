f7b537c9c4231776d001d2221e030f84c06329af
HosseinAbedZ <hossein.abedz3937@gmail.com>
2024-07-27 16:21:55 +0330

    Init Project

---
+++ Reactivities/Controllers/ActivitiesController.cs
@@ -0,0 +1,31 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistance;
+using System.Reflection.Metadata.Ecma335;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _Context;
+
+        public ActivitiesController(DataContext context)
+        {
+            _Context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _Context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _Context.Activities.FindAsync(id);
+        }
+
+    }
+}

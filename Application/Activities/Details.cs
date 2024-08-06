7f1ef46ebef9e1a7f8e8723527771b402e0c0254
HosseinAbedZ <hossein.abedz3937@gmail.com>
2024-08-06 15:59:45 +0330

    Add OrderDetails

---
+++ Application/Activities/Details.cs
@@ -0,0 +1,29 @@
+﻿using Domain;
+using MediatR;
+using Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+    public class Details
+    {
+        public class Query : IRequest<Activity>
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler(DataContext context) : IRequestHandler<Query, Activity>
+        {
+            public DataContext _Context { get; } = context;
+
+            public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _Context.Activities.FindAsync(request.Id);
+            }
+        }
+    }
+}

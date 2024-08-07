74fead66785a77eb6d7660411c806549be17f6bf
HosseinAbedZ <hossein.abedz3937@gmail.com>
2024-08-07 16:13:10 +0330

    Add DeleteActivity

---
+++ Application/Activities/Delete.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+    public class Delete
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler(DataContext context) : IRequestHandler<Command>
+        {
+            public DataContext _Context { get; } = context;
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _Context.Activities.FindAsync(request.Id);
+                if (activity != null)
+                {
+                    _Context.Activities.Remove(activity);
+                    await _Context.SaveChangesAsync();
+                }
+                    
+                return Unit.Value;
+            }
+        }
+    }
+}
